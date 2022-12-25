d1281e0e9d3fb04b54f2fef70025db90e2a081fc
Abisekh Dhakal <rupakdhakal98@gmail.com>
2022-12-25 21:48:05 +0545

    second

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,18 +1,52 @@
-const User =require("../models/userModel");
-exports.registerUser=async(req,res,next)=>{
-  
-        const{name,email,password}=req.body;
-        const user=await User.create({
-            name,
-            email,
-            password,
-            profile_pic:{
-            public_id:"default",
-            url:"default",
-            },
-        });
-    return res.status(200).json({
-        message:"User Registered",
-        user,
+const User = require("../models/userModel");
+exports.registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+      profile_pic: {
+        public_id: "default",
+        url: "default",
+      },
     });
+    return res.status(200).json({
+      message: "User Registered",
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: errorMessage,
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    return res.json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+  const token = user.getToken();
+res.cookie("token",token,{
+    expires:new Date(Date.now()+5*24*60*60*1000),
+    httpOnly:true,
+});
+  return res.status(200).json({
+    success: true,
+    user,
+   
+  });
 };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const validator =require("validator");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt= require ("jsonwebtoken");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,32 +11,45 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please Enter your email"],
-    validator:[validator.email,"please enter a valid email"]
+    validator: [validator.email, "please enter a valid email"],
+    unique: true,
   },
   profile_pic: {
-    public_id:{
-    type: String,
-    default: "default.png",
-  },
-  url: 
-       {
-        type: String,
-        default: "default.png",
-      },
+    public_id: {
+      type: String,
+      default: "default.png",
     },
-    
-  
+    url: {
+      type: String,
+      default: "default.png",
+    },
+  },
+
   password: {
     type: String,
-    require:[true,"Please Enter your password"],
-    minLength:[8,"Passwordmust be greather than 8 characters"],
+    require: [true, "Please Enter your password"],
+    minLength: [8, "Passwordmust be greather than 8 characters"],
   },
-  role: 
-    {
-      
-        type: String,
-        default: "user",
-      
+  role: {
+    type: String,
+    default: "user",
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.getToken= function(){
+  const token=  jwt.sign({id:this._id}, "secretkey",{
+   expiresIn:"1d",
+  });
+  return token;
+};
 module.exports = mongoose.model("User", userSchema);
